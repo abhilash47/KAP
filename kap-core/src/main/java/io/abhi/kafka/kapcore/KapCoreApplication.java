2513672d71e259052303298cf5f9f88fbe3dfadd
Abhilash Gajarla <abhi.gajarla@gmail.com>
2023-07-07 21:44:55 -0500

    Inital Commit .. !

---
+++ kap-core/src/main/java/io/abhi/kafka/kapcore/KapCoreApplication.java
@@ -0,0 +1,13 @@
+package io.abhi.kafka.kapcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapCoreApplication.class, args);
+	}
+
+}
---
+++ kap-core/src/test/java/io/abhi/kafka/kapcore/KapCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.abhi.kafka.kapcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
